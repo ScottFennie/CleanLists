6dc6bd3226b478ddf737e41e71bc9429f8f49f94
Scott Fennie <s.fennie7@gmail.com>
2021-09-04 13:51:22 -0600

    messing with the looks

--- app/Models/MainCardItem.js
+++ app/Models/MainCardItem.js
@@ -13,14 +13,14 @@ export class MainCardItem {
         return /*html*/ `
 
         <div class="col-lg-2 d-flex justify-content-center">
-        <div class="card text-dark bg-light my-4" style="max-width: 18rem;">
-            <div class="card-header text-center"><h6 class="text-primary">${this.title}</h6></div>
+        <div class="card text-dark bg-secondary my-4" style="max-width: 18rem;">
+            <div class="card-header text-center bg-light shadow"><h6 class="text-primary">${this.title}</h6></div>
             <div class="card-body">
                 <div>
                 ${this.ToDo}
                 </div>
                 <form class=" d-flex justify-content-end" onsubmit="app.toDoItemsController.createToDoItem('${this.id}')">
-                        <input type="text" class=" f-height form-control me-2 my-2" id="title" placeholder="Add New Item Here">
+                        <input type="text" class=" f-height form-control me-2 my-2" id="title" placeholder="Add New Task">
                         <button type="submit" class=" f-height btn btn-primary my-2 py-1">+</button>
                 </form>
             </div>
