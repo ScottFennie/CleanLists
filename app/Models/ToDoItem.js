ec0c338af004efa61e924ae2d8a0ff13977d2226
Scott Fennie <s.fennie7@gmail.com>
2021-09-04 16:35:48 -0600

    changed check box to sliders

--- app/Models/ToDoItem.js
+++ app/Models/ToDoItem.js
@@ -14,10 +14,12 @@ export class ToDoItem {
         return /*html*/ `
 
         <ul class="list-group bg-white shadow my-2">
+        <div class="form-check form-switch">
         <li class="list-group-item">
-          <input class="form-check-input me-1" type="checkbox" value="false" aria-label="...">
+          <input class="form-check-input me-1" type="checkbox" id="flexSwitchCheckDefault"value="" aria-label="...">
           ${this.title}
         </li>
+        </div>
         </ul>
         `
     }
